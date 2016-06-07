b4f62cb0921e331116a1872ee70dd03ff81d1d70
Guest User <aleah.luk@gmail.com>
2016-06-07 12:25:23 +0800

    removed repetitions, inspired by fellow classmate

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,47 +1,12 @@
 def to_roman(num)
-    digits = num.to_s.chars.map(&:to_i)
-    length = num.to_s.length
-    x = 0
-    length_arr = Array.new
-    (1..length).each do
-        x += 1
-        length_arr = length_arr << x
-    end
-    numhash = Hash[length_arr.reverse.zip(digits)]
+    number = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    roman = ["M", "DM", "D", "CD", "C", "LC", "L", "XL", "X", "XI", "V", "IV", "I"]
     answer = ""
-    if numhash.has_key?(4)
-        answer << "M" * numhash[4]
-    end
-    if numhash.has_key?(3)
-        if numhash[3] < 4
-            answer << "C" * numhash[3]
-            elsif numhash[3] == 4
-            answer << "CD"
-            elsif numhash[3] == 9
-            answer << "CM"
-            elsif numhash[3] >= 5
-            answer << "D" + ( "C" * (numhash[3]-5) )
+    for i in 0...number.length
+        while num >= number[i]
+            answer << roman[i]
+            num = num - number[i]
         end
-    end
-    if numhash.has_key?(2)
-        if numhash[2] < 4
-            answer << "X" * numhash[2]
-            elsif numhash[2] == 4
-            answer << "XL"
-            elsif numhash[2] == 9
-            answer << "XC"
-            elsif numhash[2] >= 5
-            answer << "L" + ( "X" * (numhash[2]-5) )
-        end
-    end
-    if numhash[1] < 4
-        answer << "I" * numhash[1]
-        elsif numhash[1] == 4
-        answer << "IV"
-        elsif numhash[1] == 9
-        answer << "IX"
-        elsif numhash[1] >= 5
-        answer << "V" + ( "I" * (numhash[1]-5) )
     end
     return answer
 end
